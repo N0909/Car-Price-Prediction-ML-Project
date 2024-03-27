620b5a8569d7329642d0358e8b0b617ba0cac0ae
Nikhil <143796671+N0909@users.noreply.github.com>
2024-03-27 18:03:41 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np 
 import pickle
 
-model = pickle.load(open(r'model.pkl','rb'))
-data = pickle.load(open(r'data.pkl','rb'))
+model = pickle.load(open(r'car-price-prediction-ml-project/model.pkl','rb'))
+data = pickle.load(open(r'car-price-prediction-ml-project/data.pkl','rb'))
 
 st.title('Car Price Prediction')
 
